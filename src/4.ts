a1c020fbd3cc94168f7570080a8d8a40279d7dce
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2024-11-03 15:50:40 +0100

    Finish 4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,58 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  public abstract openDoor(key: Key): void;
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Person has entered the house.');
+    } else {
+      console.log('The door is closed. Person cannot enter.');
+    }
+  }
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('Door is now open.');
+    } else {
+      console.log('Invalid key. The door remains closed.');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +61,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
