1daa92d64e9b373510c0939695a35769a126751f
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2024-11-03 13:23:03 +0100

    finish 2

--- src/2.ts
+++ src/2.ts
@@ -15,24 +15,29 @@
 */
 
 class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
-
-  getEmployeeDetails() {
+  public constructor(
+    public name: string,
+    public department: string,
+    public salary: number
+  ) { }
+  
+    getEmployeeDetails(): string {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
   }
 }
 
+
+
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary);
+  
+    this.salary += 10000;
+  }
+
+  getManagerDetails(): string {
+    return `Manager Details - ${this.getEmployeeDetails()}`;
+  }
 }
 
 
